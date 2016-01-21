36044bfe54743dccf265957999cc24606206b5b3
bortolamiolalessandr <alebortola@gmail.com>
2016-01-21 10:14:52 +0100

    Ciao

---
+++ ProgettoGrafica/src/ProgettoGrafica/GraficaProva.java
@@ -0,0 +1,55 @@
+package ProgettoGrafica;
+
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.SWT;
+
+public class GraficaProva {
+
+	protected Shell shell;
+
+	/**
+	 * Launch the application.
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		try {
+			GraficaProva window = new GraficaProva();
+			window.open();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * Open the window.
+	 */
+	public void open() {
+		Display display = Display.getDefault();
+		createContents();
+		shell.open();
+		shell.layout();
+		while (!shell.isDisposed()) {
+			if (!display.readAndDispatch()) {
+				display.sleep();
+			}
+		}
+	}
+
+	/**
+	 * Create contents of the window.
+	 */
+	protected void createContents() {
+		shell = new Shell();
+		shell.setEnabled(false);
+		shell.setSize(450, 300);
+		shell.setText("SWT Application");
+		
+		Button btnAggiungi = new Button(shell, SWT.NONE);
+		btnAggiungi.setEnabled(false);
+		btnAggiungi.setBounds(332, 87, 90, 30);
+		btnAggiungi.setText("aggiungi");
+
+	}
+}
